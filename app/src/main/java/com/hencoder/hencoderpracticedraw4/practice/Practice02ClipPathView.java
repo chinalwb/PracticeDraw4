5de6c513d46cb2782d6f3ae41c5a0f46ed3e0f49
Rain Liu <liuwenbin168@126.com>
2018-05-31 13:15:18 +0800

    正弦函数曲线绘制

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -1,5 +1,8 @@
 package com.hencoder.hencoderpracticedraw4.practice;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,9 +13,13 @@ import android.graphics.Path;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 import com.hencoder.hencoderpracticedraw4.R;
+
+import java.security.PrivilegedAction;
 
 public class Practice02ClipPathView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -22,6 +29,18 @@ public class Practice02ClipPathView extends View {
     Path path1 = new Path();
     Path path2 = new Path();
     int bw, bh;
+    int bx = 200;
+    int by = 400;
+    int piSize = 150 * 2;
+
+    final double PI = Math.PI;
+
+    int[] ds = new int[] {0, 10, 20, 30, 45, 50, 60, 70, 80, 90, 100, 110, 120, 130, 135, 140, 150, 160, 170, 180,
+            190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360};
+    Point[] points = new Point[ds.length];
+    int degreeIndex = 0;
+    ObjectAnimator animator = ObjectAnimator.ofInt(this, "degreeIndex", 0, ds.length - 1);
+
     public Practice02ClipPathView(Context context) {
         super(context);
     }
@@ -35,41 +54,58 @@ public class Practice02ClipPathView extends View {
     }
 
     {
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
-        bw = bitmap.getWidth();
-        bh = bitmap.getHeight();
-        int cx1 = point1.x + 200;
-        int cy1 = point1.y + 200;
-        path1.addCircle(cx1, cy1, 150, Path.Direction.CW);
+        path1.moveTo(bx, by);
+        path1.rLineTo(piSize, 0);
+        path1.rLineTo(piSize, 0);
 
-        int cx2 = point2.x + 200;
-        int cy2 = point2.y + 200;
-        path2.addCircle(cx2, cy2, 150, Path.Direction.CW);
-        path2.setFillType(Path.FillType.INVERSE_WINDING);
-//        path2.addCircle();
+
+        path2.moveTo(bx, by);
+        for (int i = 0; i < ds.length; i++) {
+            double d = ds[i];
+            double angle = (d / 180) * PI;
+            double dx = d / 180 * piSize;
+            double dy = Math.sin(angle) * piSize / 2;
+            Point point = new Point((int) (bx + dx), (int) (by - dy));
+            if (d == 360) {
+                Log.e("x", "dx == " + dx + ", dy == " + dy);
+            }
+            points[i] = point;
+            path2.lineTo(point.x, point.y);
+        }
+
+        animator.setStartDelay(1000);
+        animator.setDuration(5000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
     }
 
+    public void setDegreeIndex(int degreeIndex) {
+        this.degreeIndex = degreeIndex;
+        invalidate();
+    }
+
+    @TargetApi(21)
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        Paint paint2 = new Paint();
-//        paint2.setStrokeWidth(10);
-//        canvas.drawPoint(point1.x + bw, point1.y + bh, paint2);
-//        paint2.setColor(Color.RED);
-//        canvas.drawPoint(point1.x + 200, point1.y + 200, paint2);
-
-        canvas.save();
-        canvas.clipPath(path1);
-        paint.setAlpha(0x40);
-        canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
-        canvas.restore();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(10);
+        canvas.drawPath(path1, paint);
 
 
-        canvas.save();
-        paint.setAlpha(255);
-        canvas.clipPath(path2);
-        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
-        canvas.restore();
+        paint.setColor(Color.BLUE);
+        canvas.drawPath(path2, paint);
+
+        paint.setColor(Color.CYAN);
+        canvas.drawCircle(points[degreeIndex].x, points[degreeIndex].y, 12, paint);
+
+        paint.setColor(Color.RED);
+        canvas.drawCircle(bx, by, 8, paint);
+        canvas.drawCircle(bx + piSize, by, 8, paint);
+        canvas.drawCircle(bx + piSize * 2, by, 8, paint);
     }
 }
